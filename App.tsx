3531943116975fc26fb4337cf8433fade5ee1fa9
vinodsonu <vinodnandimandalam.com>
2020-07-09 15:59:36 +0530

    react-native was started

---
+++ App.tsx
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment, useState} from 'react';
+import {StyleSheet, View, Text, Alert, Image} from 'react-native';
+
+import NavigationRouter from './app/navigation/NavigationRouter';
+
+const App = () => {
+  const [noOfLines, setNoOfLines] = useState(1);
+  const handleOnPress = () => setNoOfLines(noOfLines + 1);
+
+  return (
+    <Fragment>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {/* <Text numberOfLines={noOfLines} onPress={handleOnPress}>
+          hello world!. hi, this is ramakrishna i am new to react-native i am
+          learning react-native hello world!. hi, this is ramakrishna i am new
+          to react-native i am learning react-native hello world!. hi, this is
+          ramakrishna i am new to react-native i am learning react-native hello
+          world!. hi, this is ramakrishna i am new to react-native i am learning
+          react-native hello world!. hi, this is ramakrishna i am new to
+          react-native i am learning react-native hello world!. hi, this is
+          ramakrishna i am new to react-native i am learning react-native hello
+          world!. hi, this is ramakrishna i am new to react-native i am learning
+          react-native hello world!. hi, this is ramakrishna i am new to
+          react-native i am learning react-native hello world!. hi, this is
+          ramakrishna i am new to react-native i am learning react-native hello
+          world!. hi, this is ramakrishna i am new to react-native i am learning
+          react-native hello world!. hi, this is ramakrishna i am new to
+          react-native i am learning react-native hello world!. hi, this is
+          ramakrishna i am new to react-native i am learning react-native
+        </Text> */}
+        <Image
+          source={{
+            uri: 'https://picsum.photos/200/300',
+            width: 200,
+            height: 200,
+          }}
+        />
+      </View>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
